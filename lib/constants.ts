50c65628f5b22acd64712fda69d18e95c823116d
adrgarcha <adrgarcha@alum.us.es>
2024-12-27 23:50:14 +0100

    feat: add navbar links

---
+++ lib/constants.ts
@@ -0,0 +1,22 @@
+export const links = [
+   {
+      id: 1,
+      label: 'Sobre mí',
+      url: '/#about',
+   },
+   {
+      id: 2,
+      label: 'Experiencia',
+      url: '/#experience',
+   },
+   {
+      id: 3,
+      label: 'Proyectos',
+      url: '/#projects',
+   },
+   {
+      id: 4,
+      label: 'Contacto',
+      url: '/#contact',
+   },
+];
