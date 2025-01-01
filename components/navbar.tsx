3a24643baed9d462662eec82434ce0811eca85fd
adrgarcha <adrgarcha@alum.us.es>
2025-01-01 15:00:07 +0100

    feat: center navbar items and make dropdown menu responsive

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -14,7 +14,7 @@ export default function Navbar() {
 
    return (
       <header className="sticky md:fixed md:left-1/2 md:-translate-x-1/2 mt-6 z-10 dark:text-tertiary">
-         <nav className="flex gap-x-2 md:gap-x-6 bg-foreground px-6 md:px-8 py-2 md:py-4 rounded-full border border-secondary dark:border-tertiary">
+         <nav className="flex justify-center gap-x-2 md:gap-x-6 bg-foreground px-6 md:px-8 py-2 md:py-4 rounded-full border border-secondary dark:border-tertiary">
             <ul className="flex items-center gap-x-2 md:gap-x-4 font-semibold text-xs md:text-base">
                {links.map(link => (
                   <li key={link.id}>
@@ -34,7 +34,7 @@ export default function Navbar() {
                   </button>
                </DropdownMenu.Trigger>
                <DropdownMenu.Portal>
-                  <DropdownMenu.Content className="z-20 min-w-32 rounded-md border border-secondary dark:border-tertiary bg-foreground p-1 shadow-lg">
+                  <DropdownMenu.Content className="z-20 min-w-24 md:min-w-32 rounded-md border border-secondary dark:border-tertiary bg-foreground p-1 shadow-lg">
                      {locales.map(locale => (
                         <DropdownMenu.Item
                            key={locale.id}
