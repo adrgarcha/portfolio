a263ebe7ab4aa19becd8585646a37709d5de2a6c
adrgarcha <adrgarcha@alum.us.es>
2025-01-02 17:16:28 +0100

    fix: modify variant styles to work with tailwind

--- components/button.tsx
+++ components/button.tsx
@@ -6,9 +6,11 @@ interface ButtonProps {
 export default function Button({ variant = 'primary', children }: ButtonProps) {
    return (
       <button
-         className={`flex items-center gap-x-2 px-4 md:px-5 py-2 bg-${variant} ${
-            variant === 'primary' ? 'text-foreground' : 'text-tertiary dark:hover:bg-background'
-         } border border-${variant} text-xs md:text-base rounded-lg font-medium [&>svg]:size-4 [&>svg]:md:size-6 hover:bg-foreground hover:text-${variant} transition-colors`}>
+         className={`flex items-center gap-x-2 px-4 md:px-5 py-2 border text-xs md:text-base rounded-lg font-medium [&>svg]:size-4 [&>svg]:md:size-6 hover:bg-foreground transition-colors ${
+            variant === 'primary'
+               ? 'text-foreground bg-primary border-primary hover:text-primary'
+               : 'text-tertiary bg-secondary border-secondary hover:text-secondary dark:hover:bg-background'
+         }`}>
          {children}
       </button>
    );
