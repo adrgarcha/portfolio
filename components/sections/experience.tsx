cd953528063ceef6518c971fd48ce0bf5edad121
adrgarcha <adrgarcha@alum.us.es>
2025-05-31 19:24:15 +0200

    feat: update experience section and make it more scalable

--- components/sections/experience.tsx
+++ components/sections/experience.tsx
@@ -1,10 +1,15 @@
 'use client';
 
 import { BriefcaseBusiness } from 'lucide-react';
-import { useTranslations } from 'next-intl';
+import { useMessages, useTranslations } from 'next-intl';
+
+const timelineMargins = ["mt-[4rem]", "mt-[20rem]", "mt-[36rem]", "mt-[52rem]"];
 
 export default function Experience() {
    const t = useTranslations('experience');
+
+   const messages = useMessages();
+   const keys = Object.keys(messages.experience).slice(1).reverse() as ["first", "second", "third", "fourth"];
 
    return (
       <section className="py-12 md:pt-24 md:pb-20" id="experience">
@@ -13,23 +18,33 @@ export default function Experience() {
             <h2 className="font-bold text-xl md:text-4xl">{t('title')}</h2>
             <div className="absolute w-screen h-[40px] md:h-[72px] mr-12 md:mr-64 bg-foreground border border-l-0 border-secondary rounded-lg rounded-l-none -z-10" />
          </div>
-         <div className="grid md:flex grid-rows-2 grid-flow-col md:justify-center gap-x-10 md:gap-x-24 mt-10">
-            <ExperienceSection role={t('first.role')} company="Golgorio" period={t('first.period')} className="md:text-right mt-16 md:mt-14">
-               {t('first.description')}
-            </ExperienceSection>
-            <div className="relative w-3 h-0 intersect:h-[618px] duration-[2000ms] transition-all intersect-full intersect-once border-r border-l border-secondary dark:border-secondary/30 row-span-2 order-first md:order-none">
-               <div className="absolute flex items-center left-1/2 -translate-x-1/2 mt-16">
-                  <div className="size-6 bg-primary border border-secondary dark:border-secondary/30 rounded-full opacity-0 intersect:opacity-100 intersect:motion-preset-bounce intersect:motion-delay-200 intersect-once" />
-                  <div className="absolute md:right-0 w-0 intersect:w-12 intersect:md:w-24 transition-all intersect-full intersect-once duration-500 delay-200 h-px bg-secondary dark:bg-secondary/30 -z-10" />
-               </div>
-               <div className="absolute flex items-center left-1/2 -translate-x-1/2 mt-80">
-                  <div className="size-6 bg-primary border border-secondary dark:border-secondary/30 rounded-full opacity-0 intersect:opacity-100 intersect:motion-preset-bounce intersect:motion-delay-500 intersect-once" />
-                  <div className="absolute w-0 intersect:w-12 intersect:md:w-24 transition-all intersect-full intersect-once duration-500 delay-500 h-px bg-secondary dark:bg-secondary/30 -z-10" />
-               </div>
+         <div className="flex md:justify-center gap-x-10 md:gap-x-24 mt-10">
+            <div className='hidden md:flex flex-col gap-y-64 mt-10'>
+               {keys.filter((_, index) => index % 2 === 0).map(key => (
+                  <ExperienceSection key={key} role={t(`${key}.role`)} company={t(`${key}.company`)} period={t(`${key}.period`)} className="md:text-right">
+                     {t(`${key}.description`)}
+                  </ExperienceSection>
+               ))}
             </div>
-            <ExperienceSection role={t('second.role')} company="Guadaltel" period={t('second.period')} className="mt-2 md:mt-[19.65rem]">
-               {t('second.description')}
-            </ExperienceSection>
+            <div className="relative w-10 md:w-3 h-0 intersect:h-[1084px] duration-[2000ms] transition-all intersect-full intersect-once border-r border-l border-secondary dark:border-secondary/30">
+               {keys.map((_, index) => (
+                  <TimelinePoint key={index} orientation={index % 2 === 0 ? "left" : "right"} className={timelineMargins[index]} />
+               ))}
+            </div>
+            <div className='flex md:hidden flex-col gap-y-[4.5rem] mt-14'>
+               {keys.map(key => (
+                  <ExperienceSection key={key} role={t(`${key}.role`)} company={t(`${key}.company`)} period={t(`${key}.period`)} className="md:text-right">
+                     {t(`${key}.description`)}
+                  </ExperienceSection>
+               ))}
+            </div>
+            <div className='hidden md:flex flex-col gap-y-64 mt-[19rem]'>
+               {keys.filter((_, index) => index % 2 !== 0).map(key => (
+                  <ExperienceSection key={key} role={t(`${key}.role`)} company={t(`${key}.company`)} period={t(`${key}.period`)}>
+                     {t(`${key}.description`)}
+                  </ExperienceSection>
+               ))}
+            </div>
          </div>
       </section>
    );
@@ -56,4 +71,13 @@ function ExperienceSection({
          <p className="lg:w-[410px] text-sm md:text-base">{children}</p>
       </section>
    );
+}
+
+function TimelinePoint({ orientation, className }: { orientation: "left" | "right"; className?: string }) {
+   return (
+      <div className={`absolute flex items-center left-1/2 -translate-x-1/2 ${className}`}>
+         <div className="size-6 bg-primary border border-secondary dark:border-secondary/30 rounded-full opacity-0 intersect:opacity-100 intersect:motion-preset-bounce intersect:motion-delay-500 intersect-once" />
+         <div className={`absolute ${orientation === "left" && "md:right-0"} w-0 intersect:w-12 intersect:md:w-24 transition-all intersect-full intersect-once duration-500 delay-500 h-px bg-secondary dark:bg-secondary/30 -z-10`} />
+      </div>
+   )
 }
