f2d3d19e4e2c5fad52861fbb515fb98d83071f98
adrgarcha <adrgarcha@alum.us.es>
2025-12-06 18:35:02 +0100

    feat: update nextjs version and enable the react compiler

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,21 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/ingest/static/:path*',
-        destination: 'https://eu-assets.i.posthog.com/static/:path*',
-      },
-      {
-        source: '/ingest/:path*',
-        destination: 'https://eu.i.posthog.com/:path*',
-      },
-    ];
-  },
-  // Required to support PostHog trailing slash API requests
-  skipTrailingSlashRedirect: true,
+   reactCompiler: true,
+   async rewrites() {
+      return [
+         {
+            source: '/ingest/static/:path*',
+            destination: 'https://eu-assets.i.posthog.com/static/:path*',
+         },
+         {
+            source: '/ingest/:path*',
+            destination: 'https://eu.i.posthog.com/:path*',
+         },
+      ];
+   },
+   // Required to support PostHog trailing slash API requests
+   skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
